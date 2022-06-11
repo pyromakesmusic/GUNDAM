ea4f276721dc9b29c004adb5299f2e037dc5b839
pyromakesmusic <tayo.castro@gmail.com>
2022-06-11 07:03:11 -0500

    initial commit. only structural

---
+++ model_alpha.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 11 06:32:51 2022
+
+@author: Ghost
+"""
+
+
+
+"""
+LIBRARIES
+
+Here come the importations of libraries.
+"""
+import numpy
+import scipy
+import ast
+import datetime
+import pandas
+
+
+"""
+GLOBAL VARIABLES
+
+Here go the declarations of global variables.
+"""
+
+
+
+"""
+FUNCTION DEFINITIONS
+
+Here go function definitions.
+"""
+
+
+
+
+
+"""
+UNIT TESTS
+
+Here go unit tests.
+"""
+print("Hello World!")
